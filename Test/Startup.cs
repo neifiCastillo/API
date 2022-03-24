b917f57e6ef3772e835a6e7e18e1102adfe1579d
Neifi Castillo <neifi03128@gmail.com>
2022-03-24 17:59:53 -0400

    second commit cors

--- Test/Startup.cs
+++ Test/Startup.cs
@@ -17,6 +17,7 @@ namespace Test
 {
     public class Startup
     {
+        private readonly string MyPolice = "MyCors";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,6 +28,13 @@ namespace Test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(o => o.AddPolicy(MyPolice, builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
 
             services.AddDbContext<PruebaTI>();
 
@@ -40,6 +48,8 @@ namespace Test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(MyPolice);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
